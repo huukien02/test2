748229bd11edbf353248a44015df3f971b93667a
huukien02 <111216192+huukien02@users.noreply.github.com>
2022-10-31 10:45:05 +0700

    Update Footer.js
    Signed-off-by: huukien02 <111216192+huukien02@users.noreply.github.com>

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
 
             <div className='mid'>
                 <ul>
-                    <li>Home</li>
+                    <li>Home </li>
                     <li>Features</li>
                     <li>Courses</li>
                     <li>Blog</li>
